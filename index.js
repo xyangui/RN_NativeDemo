efd520d825a5720c7d58f6aaf6b7ae15fdb50375
ccrimsonn <luochenlong_1@hotmail.com>
2018-03-22 21:15:42 +1100

    ios http request config--ivan

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ import { AppRegistry } from 'react-native';
 // import App from './App';
 // AppRegistry.registerComponent('ReactNativeDemo', () => App);
 
-import apiApp from './getapi/apiApp';
-AppRegistry.registerComponent('ReactNativeDemo', () => apiApp);
+// import apiApp from './getapi/apiApp';
+// AppRegistry.registerComponent('ReactNativeDemo', () => apiApp);
 
 // import loginApp from './persistentlogin/loginApp';
 // AppRegistry.registerComponent('ReactNativeDemo', () => loginApp);
@@ -14,6 +14,6 @@ AppRegistry.registerComponent('ReactNativeDemo', () => apiApp);
 // import Pagejump from './pagejump/App';
 // AppRegistry.registerComponent('ReactNativeDemo', () => Pagejump);
 
-// import TabDemo from './BottomTab/App';
-// AppRegistry.registerComponent('ReactNativeDemo', () => TabDemo);
+import TabDemo from './BottomTab/App';
+AppRegistry.registerComponent('ReactNativeDemo', () => TabDemo);
 
